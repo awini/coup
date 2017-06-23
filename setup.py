dbdf4abf80d31d3ff3baf2712eced7f88800291d
wind <aa.veter@gmail.com>
2017-06-23 18:37:20 +0300

    js

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from distutils.core import setup
 
 setup(
     name='coup',
-    version='0.0.18',
+    version='0.0.19',
     description='BP Compas Coup Project',
     author='BP Compas Andrew Andreev-Krasnoselsky',
     author_email='aa.veter@gmail.com',
-    packages=['coup', 'coup.objecter_core', 'coup.swift', 'coup.py', 'coup.php', 'coup.common'],
+    packages=['coup', 'coup.objecter_core', 'coup.swift', 'coup.py', 'coup.php',
+              'coup.common', 'coup.js'],
     scripts=[],
     package_data={'coup': [
         'external/highlight/styles/*.css',
