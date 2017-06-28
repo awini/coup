ea70dae273dacab5407374de14bae2497187f957
Андрей А. Андреев <wind@compas.ru>
2017-06-28 15:43:42 +0300

    abc to package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='coup',
-    version='0.0.27',
+    version='0.0.28',
     description='BP Compas Coup Project',
     author='BP Compas Andrew Andreev-Krasnoselsky',
     author_email='aa.veter@gmail.com',
@@ -13,5 +13,6 @@ setup(
     package_data={'coup': [
         'external/highlight/styles/*.css',
         'external/highlight/highlight.pack.js',
+        'common/*.abc'
     ]},
 )
